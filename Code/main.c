37a3c94dfc51636a413728a8a0fc123fb31a172b
elia <raiedaleve@gmail.com>
2026-06-19 10:59:25 +0300

    feat: parse PCAP packets and iterate through capture data

--- Code/main.c
+++ Code/main.c
@@ -20,19 +20,51 @@ typedef struct __attribute__((packed))
     uint32_t ts_usec;
     uint32_t incl_len;
     uint32_t orig_len;
-} pacp_packet_header_t;
+} pcap_packet_header_t;
 
 int main()
 {
+
     char *name = get_file_name();
     pcap_global_header_t global_header;
+    pcap_packet_header_t packet_header;
 
     int_Node *head = NULL;
 
     FILE *filePointer = import_file(name);
+    if (filePointer == NULL)
+    {
+        exit(1);
+        return 0;
+    }
+
+    long pos = ftell(filePointer);
+    printf("\n------>Position in file = %ld\n\n", pos);
+
     fread(&global_header, sizeof(pcap_global_header_t), 1, filePointer);
     printf("Magic Number : 0x%X\n", global_header.magic_number);
     printf("Snaplen : 0x%X\n", global_header.snaplen);
     printf("Network : 0x%X\n", global_header.network);
+
+    pos = ftell(filePointer);
+    printf("\n------>Position in file = %ld\n", pos);
+
+    unsigned int i = 0;
+    while (fread(&packet_header, sizeof(pcap_packet_header_t), 1, filePointer) != 0)
+    {
+        pos = ftell(filePointer);
+        printf("\n------>Position in file = %ld\n\n", pos);
+
+        printf("Packet Header : %d\n", i);
+        printf("Packet Size   : %d\n", packet_header.incl_len);
+        i++;
+
+        fseek(filePointer, packet_header.incl_len, SEEK_CUR);
+
+        pos = ftell(filePointer);
+        printf("\n------>Position in file = %ld\n", pos);
+    }
+
     fclose(filePointer);
+    return 0;
 }
