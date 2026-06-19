2bda186ad0d2f8e91b6971d9a7bb30d734bd14e1
elia <raiedaleve@gmail.com>
2026-06-19 09:29:19 +0300

    feat: add pcap_packet_header_t for parsing packet records

--- Code/main.c
+++ Code/main.c
@@ -14,17 +14,25 @@ typedef struct __attribute__((packed))
 
 } pcap_global_header_t;
 
+typedef struct __attribute__((packed))
+{
+    uint32_t ts_sec;
+    uint32_t ts_usec;
+    uint32_t incl_len;
+    uint32_t orig_len;
+} pacp_packet_header_t;
+
 int main()
 {
     char *name = get_file_name();
-    pcap_global_header_t packet;
+    pcap_global_header_t global_header;
 
     int_Node *head = NULL;
 
     FILE *filePointer = import_file(name);
-    fread(&packet, sizeof(pcap_global_header_t), 1, filePointer);
-    printf("Magic Number : 0x%X\n", packet.magic_number);
-    printf("Snaplen : 0x%X\n", packet.snaplen);
-    printf("Network : 0x%X\n", packet.network);
+    fread(&global_header, sizeof(pcap_global_header_t), 1, filePointer);
+    printf("Magic Number : 0x%X\n", global_header.magic_number);
+    printf("Snaplen : 0x%X\n", global_header.snaplen);
+    printf("Network : 0x%X\n", global_header.network);
     fclose(filePointer);
 }
