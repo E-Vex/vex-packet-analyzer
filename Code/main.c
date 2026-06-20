9954bebf8ce6473b5a3bb831594e8a602ea2582c
elia <raiedaleve@gmail.com>
2026-06-20 16:33:37 +0300

    feat: add PCAP magic number validation and byte swapping
    - Implement  to verify PCAP file validity.
    - Implement  to handle byte swapping (endianness).
    - Clean up debug print statements ( and file positions).

--- Code/main.c
+++ Code/main.c
@@ -22,6 +22,37 @@ typedef struct __attribute__((packed))
     uint32_t orig_len;
 } pcap_packet_header_t;
 
+void revers_magic_number(uint32_t *M)
+{
+    uint8_t byte1;
+    uint8_t byte2;
+    uint8_t byte3;
+    uint8_t byte4;
+
+    byte1 = (*M >> 24) & 0xFF;
+    byte2 = (*M >> 16) & 0xFF;
+    byte3 = (*M >> 8) & 0xFF;
+    byte4 = (*M >> 0) & 0xFF;
+
+    *M = (byte4 << 24) |
+         (byte3 << 16) |
+         (byte2 << 8) |
+         (byte1);
+}
+
+void check_magic_number(uint32_t *M)
+{
+    if (*M == 0xd4c3b2a1)
+    {
+        revers_magic_number(M);
+    }
+    else
+    {
+        printf("Error: the file is corrupted or is not a valid PCAP file\n");
+        exit(1);
+    }
+}
+
 int main()
 {
 
@@ -29,7 +60,7 @@ int main()
     pcap_global_header_t global_header;
     pcap_packet_header_t packet_header;
 
-    int_Node *head = NULL;
+    // int_Node *head = NULL;
 
     FILE *filePointer = import_file(name);
     if (filePointer == NULL)
@@ -38,31 +69,25 @@ int main()
         return 0;
     }
 
-    long pos = ftell(filePointer);
-    printf("\n------>Position in file = %ld\n\n", pos);
+    // long pos = ftell(filePointer);
+    // printf("\n------>Position in file = %ld\n\n", pos);
 
     fread(&global_header, sizeof(pcap_global_header_t), 1, filePointer);
+    check_magic_number(&(global_header.magic_number));
+
     printf("Magic Number : 0x%X\n", global_header.magic_number);
     printf("Snaplen : 0x%X\n", global_header.snaplen);
     printf("Network : 0x%X\n", global_header.network);
 
-    pos = ftell(filePointer);
-    printf("\n------>Position in file = %ld\n", pos);
-
     unsigned int i = 0;
     while (fread(&packet_header, sizeof(pcap_packet_header_t), 1, filePointer) != 0)
     {
-        pos = ftell(filePointer);
-        printf("\n------>Position in file = %ld\n\n", pos);
 
         printf("Packet Header : %d\n", i);
         printf("Packet Size   : %d\n", packet_header.incl_len);
         i++;
 
         fseek(filePointer, packet_header.incl_len, SEEK_CUR);
-
-        pos = ftell(filePointer);
-        printf("\n------>Position in file = %ld\n", pos);
     }
 
     fclose(filePointer);
