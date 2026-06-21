dcd52a3e572086aee0090c8ca933126a7f8fe636
elia <raiedaleve@gmail.com>
2026-06-21 14:51:02 +0300

    refactor: improve PCAP header parsing and endianness handling

--- Code/main.c
+++ Code/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include "file.h"
 /*--------------------------Structures-------------------------------*/
 typedef struct __attribute__((packed)) // Global Header
@@ -21,7 +22,18 @@ typedef struct __attribute__((packed)) // Packet Header
     uint32_t incl_len;
     uint32_t orig_len;
 } pcap_packet_header_t;
-/*--------------------------Structures-------------------------------*/
+typedef struct __attribute__((packed)) // SLL2 Header
+{
+    uint8_t skipped_data[18];
+    uint16_t protocol_type;
+} sll2_header_t;
+/*-------------------------------------------------------------------*/
+
+typedef enum
+{
+    BIG,
+    LITTLE
+} endian_t;
 
 /*--------------------------Functions-------------------------------*/
 void check_file_pointer(FILE *fp)
@@ -32,33 +44,30 @@ void check_file_pointer(FILE *fp)
         exit(1);
     }
 }
-void revers_magic_number(uint32_t *M)
+void swap_bytes(void *data, size_t size)
 {
-    uint8_t byte1;
-    uint8_t byte2;
-    uint8_t byte3;
-    uint8_t byte4;
+    uint8_t *bytes = (uint8_t *)data;
 
-    byte1 = (*M >> 24) & 0xFF;
-    byte2 = (*M >> 16) & 0xFF;
-    byte3 = (*M >> 8) & 0xFF;
-    byte4 = (*M >> 0) & 0xFF;
+    uint8_t tmp;
 
-    *M = (byte4 << 24) |
-         (byte3 << 16) |
-         (byte2 << 8) |
-         (byte1);
+    for (size_t i = 0; i < size / 2; i++)
+    {
+        tmp = bytes[i];
+        bytes[i] = bytes[size - 1 - i];
+        bytes[size - 1 - i] = tmp;
+    }
 }
 int check_magic_number(uint32_t *M)
 {
-    if (*M == 0x1a2b3c4d)
+    uint8_t *b = (uint8_t *)M;
+
+    if (b[0] == 0xa1 && b[1] == 0xb2 && b[2] == 0xc3 && b[3] == 0xd4)
     {
-        return 0;
+        return LITTLE;
     }
-    else if (*M == 0xd4c3b2a1)
+    else if (b[0] == 0xd4 && b[1] == 0xc3 && b[2] == 0xb2 && b[3] == 0xa1)
     {
-        revers_magic_number(M);
-        return 1;
+        return BIG;
     }
     else
     {
@@ -80,12 +89,13 @@ void read_packet_header(FILE *fp)
         fseek(fp, packet_header.incl_len, SEEK_CUR);
     }
 }
-/*--------------------------Functions-------------------------------*/
+/*------------------------------------------------------------------*/
 
 int main()
 {
     pcap_global_header_t global_header;
     pcap_packet_header_t packet_header;
+    sll2_header_t sll2_header;
 
     char *name = get_file_name();
 
@@ -99,7 +109,12 @@ int main()
     printf("Snaplen : 0x%X\n", global_header.snaplen);
     printf("Network : 0x%X\n", global_header.network);
 
-    read_packet_header(filePointer);
+    /* if (global_header.network == 0x114)
+     {
+         fread(&sll2_header, sizeof(sll2_header_t), 1, filePointer);
+     }*/
+
+    // read_packet_header(filePointer);
 
     fclose(filePointer);
     return 0;
