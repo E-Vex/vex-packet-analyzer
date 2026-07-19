f698bde5e1aec7b86756fe883eacb556ed252a52
elia <raiedaleve@gmail.com>
2026-07-19 12:31:44 +0300

    refactor(packet_reader): swap display.h include for protocol_parser.h
    - Removed unused display.h include from packet_reader.h (display
      calls were already removed from packet_reader.c)
    - Added protocol_parser/protocol_parser.h include in preparation for
      wiring the new layer-parsing functions into read_packets()

--- Code/packet_reader.h
+++ Code/packet_reader.h
@@ -5,9 +5,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "endian.h"
-#include "display.h"
 #include "protocols.h"
 #include "protocol_swap.h"
+#include "protocol_parser/protocol_parser.h"
 
 void read_packets(FILE *fp, uint32_t data_link_type, uint32_t magic_number);
 
