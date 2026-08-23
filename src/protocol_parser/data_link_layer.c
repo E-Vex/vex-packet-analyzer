6700da19142e6812e3006da523aa03045903386a
elia <raiedaleve@gmail.com>
2026-08-23 18:34:56 +0300

    docs(data_link_layer): fix typo in parse_sll2_header comment

--- src/protocol_parser/data_link_layer.c
+++ src/protocol_parser/data_link_layer.c
@@ -11,7 +11,7 @@ void parse_sll2_header(packet_context_t *ctx)
     sll2_header_t sll2_header;
     fread(&sll2_header, sizeof(sll2_header_t), 1, ctx->fp);
 
-    if (ctx->host_endianness == 1) /* if the host computer is little endain we have to swap every field the be in the correct network byte order */
+    if (ctx->host_endianness == 1) /* if the host computer is little endain we have to swap every field to be in the correct network byte order */
         swap_sll2_header(&sll2_header);
 
     ctx->next_protocol = sll2_header.protocol_type;
